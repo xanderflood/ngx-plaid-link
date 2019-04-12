2ecdc77814b9f0b2f073a313d3a932326078b454
Xander Flood <xander.t.flood@gmail.com>
2019-04-12 19:14:55 -0400

    trying something

--- src/lib/ngx-plaid-link.module.ts
+++ src/lib/ngx-plaid-link.module.ts
@@ -2,6 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgxPlaidLinkButtonComponent } from './ngx-plaid-link-button.component';
 import { NgxPlaidLinkService } from './ngx-plaid-link.service';
+import {
+  PlaidSuccessMetadata,
+  PlaidOnSuccessArgs,
+  PlaidInstitutionObject,
+  PlaidAccountObject,
+  PlaidErrorObject,
+  PlaidErrorMetadata,
+  PlaidOnExitArgs,
+  PlaidOnEventArgs,
+  PlaidEventMetadata,
+  PlaidConfig,
+} from './interfaces';
+import { PlaidLinkHandler } from './ngx-plaid-link-handler'
 
 @NgModule({
   imports: [
